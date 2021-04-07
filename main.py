cda0df3bc36c81328a0976bc45e933baee1595b5
Dmitriy Katkov <dmitriy.katkov.dk@gmail.com>
2021-04-07 10:07:52 +0500

    V2

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ while(1):
     n = int(input())        #code to print out a christmas tree to a number of steps (infinite)
     if(str(n)=="Q"):
         break;
-    for i in range(1,n+1):
-        for k in reversed(range((n)//2)):
+    for i in range(1,2*n,2):
+        for k in reversed(range(n-1)):
             print(" ",end="")
         for j in range(1,i+1):
             print("*", end="")
